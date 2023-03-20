ff877ef1b4a28b0dc27a702f0689f05ed4b312f1
fengyubiao <yubiao.feng@streamnative.io>
2023-03-20 15:10:04 +0800

    Revert "[Authenticate]  fix Invalid signature error when use Kerberos Authentication (#15121)"
    This reverts commit 6381128dbc1f903cfc912cc5ca77dda91adf7e86.

--- pulsar-broker-auth-sasl/src/test/java/org/apache/pulsar/broker/authentication/ProxySaslAuthenticationTest.java
+++ pulsar-broker-auth-sasl/src/test/java/org/apache/pulsar/broker/authentication/ProxySaslAuthenticationTest.java
@@ -24,7 +24,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.net.URI;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
@@ -65,8 +64,6 @@ public class ProxySaslAuthenticationTest extends ProducerConsumerBase {
 
 	public static File kdcDir;
 	public static File kerberosWorkDir;
-	public static File brokerSecretKeyFile;
-	public static File proxySecretKeyFile;
 
 	private static MiniKdc kdc;
 	private static Properties properties;
@@ -185,9 +182,6 @@ public class ProxySaslAuthenticationTest extends ProducerConsumerBase {
 		conf.setAuthenticationEnabled(true);
 		conf.setSaslJaasClientAllowedIds(".*" + localHostname + ".*");
 		conf.setSaslJaasServerSectionName("PulsarBroker");
-		brokerSecretKeyFile = File.createTempFile("saslRoleTokenSignerSecret", ".key");
-		Files.write(Paths.get(brokerSecretKeyFile.toString()), "PulsarSecret".getBytes());
-		conf.setSaslJaasServerRoleTokenSignerSecretPath(brokerSecretKeyFile.toString());
 		Set<String> providers = new HashSet<>();
 		providers.add(AuthenticationProviderSasl.class.getName());
 		conf.setAuthenticationProviders(providers);
@@ -214,10 +208,6 @@ public class ProxySaslAuthenticationTest extends ProducerConsumerBase {
 	@Override
 	@AfterMethod(alwaysRun = true)
 	protected void cleanup() throws Exception {
-		FileUtils.deleteQuietly(brokerSecretKeyFile);
-		Assert.assertFalse(brokerSecretKeyFile.exists());
-		FileUtils.deleteQuietly(proxySecretKeyFile);
-		Assert.assertFalse(proxySecretKeyFile.exists());
 		super.internalCleanup();
 	}
 
@@ -244,9 +234,7 @@ public class ProxySaslAuthenticationTest extends ProducerConsumerBase {
 		proxyConfig.setBrokerClientAuthenticationParameters(
 			"{\"saslJaasClientSectionName\": " + "\"PulsarProxy\"," +
 				"\"serverType\": " + "\"broker\"}");
-		proxySecretKeyFile = File.createTempFile("saslRoleTokenSignerSecret", ".key");
-		Files.write(Paths.get(proxySecretKeyFile.toString()), "PulsarSecret".getBytes());
-		proxyConfig.setSaslJaasServerRoleTokenSignerSecretPath(proxySecretKeyFile.toString());
+
 		// proxy as a server, it will use sasl to authn
 		Set<String> providers = new HashSet<>();
 		providers.add(AuthenticationProviderSasl.class.getName());
